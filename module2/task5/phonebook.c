1b4b013f29d7f66893d19a669945a5d5a106b571
ikadzev <ikadzev@mail.ru>
2025-11-10 00:09:39 +0700

    module 2 task 5
    Phonebook (module 1 task 1) with a dynamic (accidentally)cyclic doubly connected list

---
+++ module2/task5/phonebook.c
@@ -0,0 +1,149 @@
+#include "phonebook.h"
+
+AllocEntry* allocNewEntry(PhoneBook* book) {
+    AllocEntry* newEntry = malloc(sizeof(AllocEntry));
+    if (book->count == 0) {
+        book->firstEntry = newEntry;
+        newEntry->ident = book->count++;
+        newEntry->prev = newEntry;
+    } else {
+        book->lastEntry->next = newEntry;
+        newEntry->ident = book->lastEntry->ident + 1;
+        newEntry->prev = book->lastEntry;
+        book->count++;
+    }
+    book->lastEntry = newEntry;
+    newEntry->next = book->firstEntry;
+    return newEntry;
+}
+
+int getCount(const PhoneBook* book) { return book->count; }
+
+Entry getEntry(PhoneBook* book, int ident) { 
+    AllocEntry* cur = book->firstEntry;
+    while (cur->ident < ident) cur = cur->next;
+    return cur->data;
+}
+
+AllocEntry* getAE(PhoneBook* book, int ident) {
+    AllocEntry* cur = book->firstEntry;
+    while (cur->ident < ident) cur = cur->next;
+    return cur;
+}
+
+int checkIdent(PhoneBook* book, int ident) {
+    AllocEntry* cur = book->firstEntry;
+    while (cur->ident < ident) cur = cur->next;
+    return cur->ident == ident;
+}
+
+int addPB(PhoneBook* book, const char* name, const char* surname) {
+    AllocEntry* entry = allocNewEntry(book);
+    strcpy(entry->data.name, name);
+    strcpy(entry->data.second_name, surname);
+    return entry->ident;
+}
+
+int editPB(PhoneBook* book, int ident, char* args, ...) {
+    AllocEntry* entry = book->firstEntry;
+    while (entry->ident < ident) entry = entry->next;
+    if (entry == book->firstEntry && entry->ident != ident) return NO_ENTRY;
+    va_list argument_list;
+    va_start(argument_list, args);
+    while(*args != 0) {
+        switch (*(args++)) {
+        case 'n':
+            strcpy(entry->data.name, va_arg(argument_list, char*));
+            break;
+        case 's':
+            strcpy(entry->data.second_name, va_arg(argument_list, char*));
+            break;
+        case 'l':
+            strcpy(entry->data.last_name, va_arg(argument_list, char*));
+            break;
+        case 'j':
+            strcpy(entry->data.job, va_arg(argument_list, char*));
+            break;
+        case 'p':
+            strcpy(entry->data.job_post, va_arg(argument_list, char*));
+            break;
+        case 'e':
+            if (entry->data.emails_count == MAXEMAIL) return OVERFLOW_E;
+            strcpy(entry->data.emails[entry->data.emails_count++], va_arg(argument_list, char*));
+            break;
+        case 'f':
+            if (entry->data.phones_count == MAXPHONE) return OVERFLOW_P;
+            strcpy(entry->data.phones[entry->data.phones_count++], va_arg(argument_list, char*));
+            break;
+        case 't':
+            strcpy(entry->data.profiles.TG, va_arg(argument_list, char*));
+            break;
+        case 'w':
+            strcpy(entry->data.profiles.WA, va_arg(argument_list, char*));
+            break;
+        case 'i':
+            strcpy(entry->data.profiles.LI, va_arg(argument_list, char*));
+            break;
+        case 'g':
+            strcpy(entry->data.profiles.IG, va_arg(argument_list, char*));
+            break;
+        case 'E':
+            if (entry->data.emails_count <= 0) {
+                entry->data.emails_count = 0;
+                return OVERFLOW_E;
+            }
+            entry->data.emails_count--;
+            break;
+        case 'F':
+            if (entry->data.phones_count <= 0) {
+                entry->data.phones_count = 0;
+                return OVERFLOW_P;
+            }
+            entry->data.phones_count--;
+            break;
+        default:
+            va_end(argument_list);
+            return UNKNOWN_EXPRESSION;
+        }
+    }
+    va_end(argument_list);
+    return ident;
+}
+
+int entryPB(PhoneBook* book, const char* filter, int* found_indices) {
+    AllocEntry* currentEntry = book->firstEntry;
+    int found = 0;
+
+    do {
+        if (filter == NULL || 
+            strstr(currentEntry->data.name, filter) || 
+            strstr(currentEntry->data.second_name, filter) ||
+            strstr(currentEntry->data.last_name, filter) || 
+            strstr(currentEntry->data.job, filter) || 
+            strstr(currentEntry->data.job_post, filter)) {
+                found_indices[found++] = currentEntry->ident;
+            }
+        currentEntry = currentEntry->next;
+    } while (currentEntry->ident > currentEntry->prev->ident);
+
+    return found;
+}
+
+int deletePB(PhoneBook* book, int ident) {
+    if (book->count == 0) return NO_ENTRY;
+    if (checkIdent(book, ident) == 0) return NO_ENTRY;
+    AllocEntry* removable = getAE(book, ident);
+    if (book->count == 1) {
+        PhoneBook new = {0};
+        *book = new;
+        free(removable);
+        return ident;
+    }
+    if (removable == book->firstEntry) book->firstEntry = removable->next;
+    if (removable == book->lastEntry) book->lastEntry = removable->prev;
+    removable->next->prev = removable->prev;
+    removable->prev->next = removable->next;
+    free(removable);
+    book->count--;
+    return ident;
+}
---
+++ module2/task5/phonebook.h
@@ -0,0 +1,66 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdarg.h>
+
+// Constants used in phonebook.c
+#define MAXSTRING 100
+#define MAXEMAIL 10
+#define MAXPHONE 10
+
+#define OVERFLOW -1
+#define OVERFLOW_E -2
+#define OVERFLOW_P -3
+#define NO_ENTRY -4
+#define UNKNOWN_EXPRESSION -5
+
+// Types used in phonebook.c
+typedef struct IM {
+    char TG[MAXSTRING];
+    char WA[MAXSTRING];
+    char LI[MAXSTRING];
+    char IG[MAXSTRING];
+} IM;
+
+typedef struct Entry {
+    char name[MAXSTRING];
+    char second_name[MAXSTRING];
+    char last_name[MAXSTRING];
+    char job[MAXSTRING];
+    char job_post[MAXSTRING];
+    char emails[MAXEMAIL][MAXSTRING];
+    int emails_count;
+    char phones[MAXPHONE][12];
+    int phones_count;
+    IM profiles;
+} Entry;
+
+typedef struct AllocEntry {
+    int ident;
+    Entry data;
+    struct AllocEntry* next;
+    struct AllocEntry* prev;
+} AllocEntry;
+
+typedef struct PhoneBook {
+    int count;
+    AllocEntry* firstEntry;
+    AllocEntry* lastEntry;
+} PhoneBook;
+
+AllocEntry* allocNewEntry(PhoneBook* book);
+int getCount(const PhoneBook* book);
+Entry getEntry(PhoneBook* book, int ident);
+AllocEntry* getAE(PhoneBook* book, int ident);
+int checkIdent(PhoneBook* book, int ident);
+
+int addPB(PhoneBook* book, const char* name, const char* surname);
+int editPB(PhoneBook* book, int ident, char* args, ...);
+int deletePB(PhoneBook* book, int ident);
+int entryPB(PhoneBook* book, const char* filter, int* found_indices);
+
+#endif // PHONEBOOK_H
