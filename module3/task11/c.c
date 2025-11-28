fd7dfe02c56cacf5aa892915d68068a6f711560b
ikadzev <ikadzev@mail.ru>
2025-11-28 16:42:15 +0700

    module 3 task 11
    Shared memory POSIX

---
+++ module3/task11/c.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+typedef struct {
+    int data[100];
+    int count;
+    int min_val;
+    int max_val;
+} shm_t;
+
+#define SHM_NAME "/posix_shm_example"
+#define SEM_PARENT "/sem_parent"
+#define SEM_CHILD  "/sem_child"
+
+shm_t *shm;
+sem_t *sem_parent;
+sem_t *sem_child;
+
+int main() {
+    int fd = shm_open(SHM_NAME, O_RDWR, 0666);
+    shm = mmap(NULL, sizeof(shm_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close(fd);
+
+    sem_parent = sem_open(SEM_PARENT, 0);
+    sem_child  = sem_open(SEM_CHILD, 0);
+
+    while (1) {
+        sem_wait(sem_child);
+
+        int min = shm->data[0];
+        int max = shm->data[0];
+
+        for (int i = 1; i < shm->count; i++) {
+            if (shm->data[i] < min) min = shm->data[i];
+            if (shm->data[i] > max) max = shm->data[i];
+        }
+
+        shm->min_val = min;
+        shm->max_val = max;
+
+        sem_post(sem_parent);
+    }
+
+    return 0;
+}
---
+++ module3/task11/p.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <time.h>
+
+typedef struct {
+    int data[100];
+    int count;
+    int min_val;
+    int max_val;
+} shm_t;
+
+#define SHM_NAME "/posix_shm_example"
+#define SEM_PARENT "/sem_parent"
+#define SEM_CHILD  "/sem_child"
+
+shm_t *shm;
+sem_t *sem_parent;
+sem_t *sem_child;
+pid_t child_pid;
+
+volatile sig_atomic_t sets_processed = 0;
+
+void cleanup(int sig) {
+    printf("\nОбработано наборов: %d\n", sets_processed);
+
+    munmap(shm, sizeof(shm_t));
+    shm_unlink(SHM_NAME);
+
+    sem_close(sem_parent);
+    sem_close(sem_child);
+    sem_unlink(SEM_PARENT);
+    sem_unlink(SEM_CHILD);
+
+    kill(child_pid, SIGTERM);
+
+    exit(0);
+}
+
+int main() {
+    int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, sizeof(shm_t));
+
+    shm = mmap(NULL, sizeof(shm_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close(fd);
+
+    sem_parent = sem_open(SEM_PARENT, O_CREAT, 0666, 0);
+    sem_child  = sem_open(SEM_CHILD,  O_CREAT, 0666, 0);
+
+    signal(SIGINT, cleanup);
+
+    child_pid = fork();
+
+    if (child_pid == 0) {
+        execl("./child", "./child", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    while (1) {
+        int n = rand() % 10 + 5;
+        shm->count = n;
+
+        for (int i = 0; i < n; i++)
+            shm->data[i] = rand() % 100;
+
+        sem_post(sem_child);
+        sem_wait(sem_parent);
+
+        printf("min = %d, max = %d\n", shm->min_val, shm->max_val);
+        sets_processed++;
+
+        sleep(1);
+    }
+
+    return 0;
+}
