02bb74ce5f591bec71c3ad2f1fcfc1a9dfef92ca
ikadzev <ikadzev@mail.ru>
2025-12-03 19:52:20 +0700

    m3t5: main file edit; .gitignore

--- module2/task5/phonebook.c
+++ module2/task5/phonebook.c
@@ -59,54 +59,14 @@ static AllocEntry* findById(PhoneBook* book, int ident) {
     return NULL;
 }
 
-// AllocEntry* OLDallocNewEntry(PhoneBook* book) {
-//     AllocEntry* newEntry = malloc(sizeof(AllocEntry));
-//     if (book->count == 0) {
-//         book->firstEntry = newEntry;
-//         newEntry->ident = book->count++;
-//         newEntry->prev = newEntry;
-//     } else {
-//         book->lastEntry->next = newEntry;
-//         newEntry->ident = book->lastEntry->ident + 1;
-//         newEntry->prev = book->lastEntry;
-//         book->count++;
-//     }
-//     book->lastEntry = newEntry;
-//     newEntry->next = book->firstEntry;
-//     return newEntry;
-// }
-
 int getCount(const PhoneBook* book) { return book->count; }
 
-// Entry OLDgetEntry(PhoneBook* book, int ident) { 
-//     AllocEntry* cur = book->firstEntry;
-//     while (cur->ident < ident) cur = cur->next;
-//     return cur->data;
-// }
 Entry getEntry(PhoneBook* book, int ident) {
     AllocEntry* e = findById(book, ident);
     Entry empty = {0};
     return e ? e->data : empty;
 }
 
-// AllocEntry* OLDgetAE(PhoneBook* book, int ident) {
-//     AllocEntry* cur = book->firstEntry;
-//     while (cur->ident < ident) cur = cur->next;
-//     return cur;
-// }
-
-// int OLDcheckIdent(PhoneBook* book, int ident) {
-//     AllocEntry* cur = book->firstEntry;
-//     while (cur->ident < ident) cur = cur->next;
-//     return cur->ident == ident;
-// }
-
-// int OLDaddPB(PhoneBook* book, const char* name, const char* surname) {
-//     AllocEntry* entry = allocNewEntry(book);
-//     strcpy(entry->data.name, name);
-//     strcpy(entry->data.second_name, surname);
-//     return entry->ident;
-// }
 int addPB(PhoneBook* book, const char* name, const char* surname) {
     AllocEntry* entry = malloc(sizeof(AllocEntry));
     memset(entry, 0, sizeof(*entry));
@@ -121,71 +81,6 @@ int addPB(PhoneBook* book, const char* name, const char* surname) {
     return entry->ident;
 }
 
-// int OLDeditPB(PhoneBook* book, int ident, char* args, ...) {
-//     AllocEntry* entry = book->firstEntry;
-//     while (entry->ident < ident) entry = entry->next;
-//     if (entry == book->firstEntry && entry->ident != ident) return NO_ENTRY;
-//     va_list argument_list;
-//     va_start(argument_list, args);
-//     while(*args != 0) {
-//         switch (*(args++)) {
-//         case 'n':
-//             strcpy(entry->data.name, va_arg(argument_list, char*));
-//             break;
-//         case 's':
-//             strcpy(entry->data.second_name, va_arg(argument_list, char*));
-//             break;
-//         case 'l':
-//             strcpy(entry->data.last_name, va_arg(argument_list, char*));
-//             break;
-//         case 'j':
-//             strcpy(entry->data.job, va_arg(argument_list, char*));
-//             break;
-//         case 'p':
-//             strcpy(entry->data.job_post, va_arg(argument_list, char*));
-//             break;
-//         case 'e':
-//             if (entry->data.emails_count == MAXEMAIL) return OVERFLOW_E;
-//             strcpy(entry->data.emails[entry->data.emails_count++], va_arg(argument_list, char*));
-//             break;
-//         case 'f':
-//             if (entry->data.phones_count == MAXPHONE) return OVERFLOW_P;
-//             strcpy(entry->data.phones[entry->data.phones_count++], va_arg(argument_list, char*));
-//             break;
-//         case 't':
-//             strcpy(entry->data.profiles.TG, va_arg(argument_list, char*));
-//             break;
-//         case 'w':
-//             strcpy(entry->data.profiles.WA, va_arg(argument_list, char*));
-//             break;
-//         case 'i':
-//             strcpy(entry->data.profiles.LI, va_arg(argument_list, char*));
-//             break;
-//         case 'g':
-//             strcpy(entry->data.profiles.IG, va_arg(argument_list, char*));
-//             break;
-//         case 'E':
-//             if (entry->data.emails_count <= 0) {
-//                 entry->data.emails_count = 0;
-//                 return OVERFLOW_E;
-//             }
-//             entry->data.emails_count--;
-//             break;
-//         case 'F':
-//             if (entry->data.phones_count <= 0) {
-//                 entry->data.phones_count = 0;
-//                 return OVERFLOW_P;
-//             }
-//             entry->data.phones_count--;
-//             break;
-//         default:
-//             va_end(argument_list);
-//             return UNKNOWN_EXPRESSION;
-//         }
-//     }
-//     va_end(argument_list);
-//     return ident;
-// }
 int editPB(PhoneBook* book, int ident, char* args, ...) {
     AllocEntry* e = findById(book, ident);
     if (!e) return NO_ENTRY;
@@ -245,24 +140,6 @@ int editPB(PhoneBook* book, int ident, char* args, ...) {
     return ident;
 }
 
-// int OLDentryPB(PhoneBook* book, const char* filter, int* found_indices) {
-//     AllocEntry* currentEntry = book->firstEntry;
-//     int found = 0;
-
-//     do {
-//         if (filter == NULL || 
-//             strstr(currentEntry->data.name, filter) || 
-//             strstr(currentEntry->data.second_name, filter) ||
-//             strstr(currentEntry->data.last_name, filter) || 
-//             strstr(currentEntry->data.job, filter) || 
-//             strstr(currentEntry->data.job_post, filter)) {
-//                 found_indices[found++] = currentEntry->ident;
-//             }
-//         currentEntry = currentEntry->next;
-//     } while (currentEntry->ident > currentEntry->prev->ident);
-
-//     return found;
-// }
 int entryPB(PhoneBook* book, const char* filter, int* found) {
     int count = 0;
     AllocEntry* cur = book->firstEntry;
@@ -283,24 +160,6 @@ int entryPB(PhoneBook* book, const char* filter, int* found) {
     return count;
 }
 
-// int OLDdeletePB(PhoneBook* book, int ident) {
-//     if (book->count == 0) return NO_ENTRY;
-//     if (checkIdent(book, ident) == 0) return NO_ENTRY;
-//     AllocEntry* removable = getAE(book, ident);
-//     if (book->count == 1) {
-//         PhoneBook new = {0};
-//         *book = new;
-//         free(removable);
-//         return ident;
-//     }
-//     if (removable == book->firstEntry) book->firstEntry = removable->next;
-//     if (removable == book->lastEntry) book->lastEntry = removable->prev;
-//     removable->next->prev = removable->prev;
-//     removable->prev->next = removable->next;
-//     free(removable);
-//     book->count--;
-//     return ident;
-// }
 int deletePB(PhoneBook* book, int ident) {
     AllocEntry* e = findById(book, ident);
     if (!e) return NO_ENTRY;
@@ -312,17 +171,6 @@ int deletePB(PhoneBook* book, int ident) {
     return ident;
 }
 
-// void OLDfreePB(PhoneBook* book) {
-//     if (book->count == 0) return;
-//     AllocEntry* cur = book->firstEntry;
-//     AllocEntry* temp;
-//     for (int i = 0; i < book->count; i++) {
-//         temp = cur->next;
-//         free(cur);
-//         cur = temp;
-//     }
-//     return;
-// }
 void freePB(PhoneBook* book) {
     AllocEntry* cur = book->firstEntry;
     while (cur) {
